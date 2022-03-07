6dec535b52f705d6fe9408fc72885f09351d3562
O Gustavo <avoguga@gmail.com>
2022-03-07 00:15:44 -0300

    fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Toolkit from "./components/Toolkit";
 import GlobalStyles from "./styles/global";
-import increaseImg from "../src/assets/guax.png";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
-      <img src={increaseImg} alt="" />
       <Toolkit />
     </div>
   );
