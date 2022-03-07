5244576db0bf07a5f28488a6e24de2115271cd92
O Gustavo <avoguga@gmail.com>
2022-03-07 00:32:52 -0300

    changing icons

--- src/components/Toolkit/index.tsx
+++ src/components/Toolkit/index.tsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSpeechSynthesis } from "react-speech-kit";
 
-import increaseImg from "../../assets/texto.png";
-import wordSpacing from "../../assets/duas-flechas.png";
-
 import MainButton from "../MainButton";
 import Button from "../ActionButton";
 import { Animation } from "../MainButton";
@@ -47,18 +44,14 @@ const Toolkit: React.FC = () => {
   const [textSelected, setTextSelected] = useState<any>(selectedText);
 
   // Getting mouse actions to verify that text has been selected
-  document.addEventListener("selectionchange", () => {
-    setTimeout(() => {
-      setTextSelected(selectedText?.toString());
-    }, 1000);
-  });
 
   useEffect(() => {
-    if (textToBeSpeeched) {
-      if (textSelected) {
-        textToSpeech();
-      } else console.log("Button inactive");
-    }
+    document.addEventListener("selectionchange", () => {
+      setTimeout(() => {
+        setTextSelected(selectedText?.toString());
+      }, 1000);
+    });
+    if (textToBeSpeeched && textSelected) textToSpeech();
   }, [textSelected]);
 
   const textToSpeech = () => {
@@ -74,26 +67,29 @@ const Toolkit: React.FC = () => {
           <Content className={move ? `move` : ``}>
             <Button
               image={
-                "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFoAAABaCAYAAAA4qEECAAAABmJLR0QA/wD/AP+gvaeTAAAB9klEQVR4nO2cS24CMRBES1mEnDzMebJAuVBCbgGL0BIiw4SPu101rid5w+r5CQwDQwPGGGOMMcYYY4wxZo4NgAnA/rSm02MqSPi/AvgAcLhYnwDeOnrdioT/NUlK2Rkk/P+TpJKdQcL/VkkK2Rkk/O+VZIst4b8BsHtAMtYOfd/NZfynJyR7x342cqxthey+gWiPY+TR42Ju/VQItwpdGbtl5AOArwJnbBsKVxwjrY6L8/We6JsqnhVbyXWW1i/FjGNEwfEmmDfC7PYQjBtidGoC08aYXFJg2CCDQwk9NzpM5KDHhoeLHFRufNjIQUWA4SMHmSEc+YKsS2Dpy+osMp59fiZfgTX2qiIHbLFXGTlgib3qyEHv2ENEDnrFHipyUB17yMhBVeyhIwfZsR35jKzYNJFfeguYOnx0FOA3wwL88a4AX7AU4EvwAnpHHiI2S+RVx2aLvMrYrJFXFds/zhbg2w0K8A00BfiWsAJ8k2MBvm23AIaNMjikwrRBJpemMG6M0ekpmDfE7HYXChtRcFxE6W+/Sq5/aDGvo1I8I7bUvI7Kl2LrY0RqXofyYBSZeR3qo34k5nX0/u5Xyl9inNkCUv4SA/oWkPKXGDm5gJS/xBDVBaT8JcYCLyDlv8HvR7/v09pC65dldX9jjDHGGGOMMcaYMo5GWlFN6GAqrAAAAABJRU5ErkJggg=="
+                "https://img.icons8.com/ios-filled/50/000000/accessibility2.png"
               }
               click={() => setIsContainerVisible(false)}
               text="Handtalk toolkit"
             ></Button>
-
             <Button
               click={() => increaseFontSize(document.body)}
-              image={increaseImg}
+              image={
+                "https://img.icons8.com/ios-filled/50/000000/increase-font.png"
+              }
               text="Aumentar texto"
             />
             <Button
               click={() => increaseWordSpacing(document.body)}
-              image={wordSpacing}
+              image={
+                "https://img.icons8.com/external-bartama-glyph-64-bartama-graphic/64/000000/external-arrow-arrows-glyph-bartama-glyph-64-bartama-graphic-15.png"
+              }
               text="Espaçamento entre palavras"
             />
             <Button
               click={() => setTextToBeSpeeched(!textToBeSpeeched)}
               image={
-                "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABmJLR0QA/wD/AP+gvaeTAAACgElEQVR4nO2au2sVQRSHv6j3BsWbWMSAjyo2FiKK2AkBQxptBNHOwkaxEBUF/wP/EbG28QUxkMLGQkGJWAgXUQQJCCqIUYRY7C4s69k9+5hxNjPzwbLcy5wz5/yYnTdEIpFIJBKJ/F/mgI3C44QtjupddFRvb3hGT1qAC47wb/ImBZgF7gEHDfo0ymPsCXAOWEv9LRnyaZQzyMl3FWA/8EDweaqjX6PsAT5jVoAJ4DLwrcTnW2DQKWpDjICXlCffRoA54KnicwO40TH2zgyAJ+iB1mUrcBP4UcPnK+C0iSTasgO4jx5oXQEOAc9r+Fol6RBdzXMAmEEe79sIMABuA+uKjzFwiaSVOOUE8J76yVcJcBx4rdi+Ay7Qg8SHwB3gD82SLxPgvOJrDFwEtlnJpiELwBuaJ14lwCzwVSj7AbgGTNpJpRnz1BuO2n4Ct3JlPgJXSFqaVc4KAWZPUfWuiWsCDIEV4KpQtxUmSMbPskB3F8rbFsAq0jh5FDhcYTNlKRYnSAKcVGymbQTiCkmAY4qN9y3ggGLjlQDSBGKvYuNSgCHJynI6jWME7Ezfu9K39F/2eyq1HZGOKJIAWoIuBfhl2qH0CWxXbLz6BNosF70X4Kdi45UAUh/wnaSTKMPlPGASc50gIAvwCdhXEUSVOLb5DXxJHyNIn8BYsfF+JvhCsfGqD5AEWFZsvBJAQlsOrxXKb+rlcBlVGyLrhbLebYg0xcstsTYskBxImBRgU2yK5gl6WzxPsAcjeYI+GssI+nA0I+jj8QyXFySud4zdGEFfkckI+pJUxiPMC5CRvyb30JBP4wR/URJ6dFXW1Th511G9vaE31+UjkUgkEomEy18IjhrfYWjSrAAAAABJRU5ErkJggg=="
+                "https://img.icons8.com/glyph-neue/64/000000/room-sound.png"
               }
               text="Leitura por voz"
             />
